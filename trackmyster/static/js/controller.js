ff109f91a311f4a4d0e8fa8458973a7fdcdd0a9e
Kaushal Mishra <kkaushy@gmail.com>
2015-09-08 10:32:49 -0700

    changes done in map direction, added waypoints feature

--- trackmyster/static/js/controller.js
+++ trackmyster/static/js/controller.js
@@ -5,26 +5,33 @@ trackmysterApp.controller("homeCtrl", function($scope){
 });
 
 trackmysterApp.controller("liveMapCtrl", function($scope, busFactory){
-	busFactory.getBusList().then(function(response){
+	
+  busFactory.getBusList().then(function(response){
 		$scope.busList = response.data;
 	})
 	$scope.title = "Live Map";
-  $scope.loc=[
+  var loc=[
       [18.638529,73.847787],
       [18.639231,73.840045],
       [18.637055,73.825679],
       [18.646896,73.814435]
     ];
+    
+    var waypts = [];
+    for(var i=1;i<loc.length-1;i++){
+      waypts.push({
+        location:loc[i].join(),
+        stopover:false
+      });
+    }
 
-    // var waypts = [];
+    $scope.waypts= waypts;
+    $scope.loc = loc;
 
-    // // stop = new google.maps.LatLng(18.637055, 73.825679)
-    // waypts.push({
-    //     location: "pune"        
-    // });
+  $scope.getBusRoute = function (bus) {
+      console.log(bus);
+  }
 
-
-    // $scope.x = waypts;
 })
 trackmysterApp.controller("routeHistoryCtrl", function($scope){
 	
