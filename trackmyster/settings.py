f0788828292bc8d6c597b3ec50546a542f388c11
Kaushal <kkaushy@gmail.com>
2015-09-23 12:25:05 +0530

    added cross origin support

--- trackmyster/settings.py
+++ trackmyster/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'student',
     'rest_framework',
+    'corsheaders'
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -50,8 +52,10 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+
 )
 
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'trackmyster.urls'
 
 TEMPLATES = [
